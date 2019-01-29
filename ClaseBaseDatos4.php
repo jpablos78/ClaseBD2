9bddfa768f89117af637ae8ad5ed64aceb619ff9
JPablos <jpablos2011.2@gmail.com>
2019-01-29 08:03:44 -0500

    commit 20190129

--- ClaseBaseDatos4.php
+++ ClaseBaseDatos4.php
@@ -22,7 +22,7 @@ class ClaseBaseDatos4 {
     private $parametros = array(
         'connect' => true,
         'interfaz' => '',
-        'autocommit' => true,
+        'autocommit' => false,
         'json' => true,
         'debug' => false,
         'verificarPermisos' => true
@@ -37,31 +37,87 @@ class ClaseBaseDatos4 {
         $this->mssql = odbc_connect("Driver={SQL Server};Server=" . $this->servidor . ";Database=" . $this->base . ";", $this->usuario, $this->clave);
 
         if ($this->mssql) {
-            return ClaseJson2::getJson(array(
+            $this->autocommit($this->parametros['autocommit']);
+
+            $result = ClaseJson2::getJson(array(
                         "success" => 'true',
                         "message" => utf8_encode("Conexión Exitosa"))
             );
         } else {
-            return ClaseJson2::getJson($this->getError());
+            $result = $this->getError();
         }
+
+        if ($this->parametros['json']) {
+            return ClaseJson2::getJson($result);
+        }
+
+        return $result;
+
+//        if ($this->mssql) {
+//            return ClaseJson2::getJson(array(
+//                        "success" => 'true',
+//                        "message" => utf8_encode("Conexión Exitosa"))
+//            );
+//        } else {
+//            return ClaseJson2::getJson($this->getError());
+//        }
     }
 
-    public function query($query) {
-        $resp = odbc_exec($this->mssql, $query);
+    public function desconectarse() {
+        odbc_close($this->mssql);
+    }
 
-        if (!odbc_error()) {
-            while ($row = odbc_fetch_array($resp)) {
-                $registros[] = array_map('utf8_encode', $row);
+    public function query($query, $parametros = '') {
+        print_r($this->parametros);
+
+        if (is_array($parametros)) {
+            $this->setParametros($parametros);
+        }
+
+        echo '<br>';
+
+        print_r($this->parametros);
+
+        if ($this->parametros['connect']) {
+            $result = $this->conectarse();
+        }
+
+        if ($this->mssql) {
+            $resp = odbc_exec($this->mssql, $query);
+
+            $mensaje = '';
+            $ok = '';
+
+            if (!odbc_error()) {
+                if ($this->parametros['autocommit']) {
+                    $this->commit();
+                }
+
+                while ($row = odbc_fetch_array($resp)) {
+                    $registros[] = array_map('utf8_encode', $row);
+                }
+
+                $result = array(
+                    "success" => 'true',
+                    "ok" => $ok,
+                    "message" => $mensaje,
+                    "data" => $registros
+                );
+            } else {
+                $result = $this->getError();
+
+                if ($this->parametros['autocommit']) {
+                    $this->rollback();
+                }
             }
 
-            $result = array(
-                "error" => 'N',
-                "ok" => $ok,
-                "message" => $mensaje,
-                "data" => $registros
-            );
-        } else {
-            $result = $this->getError();
+            if ($this->parametros['json']) {
+                $result = ClaseJson2::getJson($result);
+            }
+
+            if ($this->parametros['connect']) {
+                $this->desconectarse();
+            }
         }
 
         return $result;
@@ -71,8 +127,45 @@ class ClaseBaseDatos4 {
         return array(
             "success" => 'false',
             "message" => utf8_encode(odbc_error() . ' - ' . odbc_errormsg())
-//"message" => (odbc_error() . ' - ' . odbc_errormsg())
         );
+    }
+
+    private function autocommit($autocommit = false) {
+        odbc_autocommit($this->mssql, $autocommit);
+    }
+
+    private function commit() {
+        odbc_commit($this->mssql);
+    }
+
+    private function rollback() {
+        odbc_rollback($this->mssql);
+    }
+
+    public function setParametros($parametros) {
+        if (array_key_exists('connect', $parametros)) {
+            $this->parametros['connect'] = $parametros['connect'];
+        }
+
+        if (array_key_exists('interfaz', $parametros)) {
+            $this->parametros['interfaz'] = $parametros['interfaz'];
+        }
+
+        if (array_key_exists('autocommit', $parametros)) {
+            $this->parametros['autocommit'] = $parametros['autocommit'];
+        }
+
+        if (array_key_exists('json', $parametros)) {
+            $this->parametros['json'] = $parametros['json'];
+        }
+
+        if (array_key_exists('debug', $parametros)) {
+            $this->parametros['debug'] = $parametros['debug'];
+        }
+
+        if (array_key_exists('verificarPermisos', $parametros)) {
+            $this->parametros['verificarPermisos'] = $parametros['verificarPermisos'];
+        }
     }
 
 }
--- index4.php
+++ index4.php
@@ -5,17 +5,47 @@ include_once 'ClaseBaseDatos4.php';
 
 $objetoBaseDatos = new ClaseBaseDatos4();
 
-$result = $objetoBaseDatos->conectarse();
-
-echo $result;
-
-$query = "SELECT TOP 100 cci_empresa, cci_sucursal
-          FROM BIZ_GEN..TB_GEN_CLIPROV, ";
-
-$result = $objetoBaseDatos->query($query);
-
+//$result = $objetoBaseDatos->conectarse();
+//
+//echo $result;
+//
+//$query = "SELECT TOP 100 cci_empresa, cci_sucursal
+//          FROM BIZ_GEN..TB_GEN_CLIPROV ";
+//
+//$result = $objetoBaseDatos->query($query);
+//
 //print_r($result);
+//
+//echo $result;
 
+/*
+  $query = "SELECT TOP 100 us_login, us_nombres
+  FROM wp_usuarios ";
+
+  $parametros = array(
+  'connect' => true
+  );
+
+  echo '<br>';
+
+  $result = $objetoBaseDatos->query($query);
+
+  print_r($result);
+
+  echo $result;
+ */
+$parametros = array(
+    'connect' => false,
+    'json' => false
+);
+
+$query = "SELECT TOP 100 us_login, us_nombres
+  FROM wp_usuarios ";
+
+$objetoBaseDatos->setParametros($parametros);
+$result = $objetoBaseDatos->conectarse();
+echo $result;
+$result = $objetoBaseDatos->query($query);
 echo $result;
 
 
